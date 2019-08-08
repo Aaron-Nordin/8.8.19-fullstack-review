65c5ec7437b29109876af3ecd3dd215df6be2ff9
Aaron <nordin.aaron@gmail.com>
2019-08-08 12:12:02 -0600

    logo, routing, redux

--- src/components/Transfer/Transfer.js
+++ src/components/Transfer/Transfer.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react'
-import "./Transfer.css"
+import React, { Component } from "react";
+import "./Transfer.css";
+import { Link } from "react-router-dom";
 
 export default class Transfer extends Component {
-    state = {
-        account: "",
-        amount: "",
-        transactionType: ""
-    }
-    render() {
-        return (
-            <div className="Transfer">
-                Transfer
-            </div>
-        )
-    }
+  state = {
+    account: "",
+    amount: "",
+    transactionType: ""
+  };
+  render() {
+    return (
+      <div className="Transfer">
+        Transfer
+        <Link to="/dashboard">
+          <button>Cancel</button>
+        </Link>
+        <button>Submit</button>
+      </div>
+    );
+  }
 }
